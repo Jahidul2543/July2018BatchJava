fac3fd55f1fc643b2d81aef73f31a38baed050c4
Jahidul2543 <34196568+Jahidul2543@users.noreply.github.com>
2018-12-04 11:59:35 -0500

    Create ScanXan.java

---
+++ src/test/java/InputStream/ScanXan.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class ScanXan {
+    public static void main(String[] args) throws IOException {
+
+        Scanner s = null;
+
+        try {
+            s = new Scanner(new BufferedReader(new FileReader("O:\\EVERYONE\\JJ\\New folder\\JavaBasic\\src\\main\\java\\io\\xanadu.txt")));
+
+            while (s.hasNext()) {
+                System.out.println(s.next());
+            }
+        } finally {
+            if (s != null) {
+                s.close();
+            }
+        }
+    }
+}
